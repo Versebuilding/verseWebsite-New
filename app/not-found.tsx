a18b2e2bcbfa0d9dc4eea069b80dc46accf58a63
kooroshFarv <farvardin.koorosh@gmail.com>
2025-08-05 23:20:01 -0400

    favicon added

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,7 +6,7 @@ const notFoundPage = () => {
   return (
  <div className="min-h-screen flex flex-col items-center justify-center text-center bg-black text-white px-4">
       <h1 className="text-5xl font-bold mb-4">404</h1>
-      <p className="text-lg mb-6">The realm you're seeking doesn’t exist.</p>
+      <p className="text-lg mb-6">The realm you&apos;re seeking doesn’t exist.</p>
       <Button className='bg-white p-4'>
       <Link href="/" className="text-gray-800 ">Return to Home</Link>
       </Button>

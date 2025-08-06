74f6174437c5ad28f9571e735f9cd5509e17098b
kooroshFarv <farvardin.koorosh@gmail.com>
2025-08-05 23:12:11 -0400

    Not found page added

---
+++ app/not-found.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Link from 'next/link'
+import { Button } from './components/ui/button'
+
+const notFoundPage = () => {
+  return (
+ <div className="min-h-screen flex flex-col items-center justify-center text-center bg-black text-white px-4">
+      <h1 className="text-5xl font-bold mb-4">404</h1>
+      <p className="text-lg mb-6">The realm you're seeking doesn’t exist.</p>
+      <Button className='bg-white p-4'>
+      <Link href="/" className="text-gray-800 ">Return to Home</Link>
+      </Button>
+    </div>  
+    )
+}
+
+export default notFoundPage
